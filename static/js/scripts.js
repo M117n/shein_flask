0b5f80c8e756b146d5be4a258fcfd255ea543c10
Martin M <maartin.arriola117@gmail.com>
2024-10-01 16:50:50 -0600

    tr.appendChild(tdActions) missin in JS.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -71,6 +71,7 @@ async function mostrarResultado() {
                 tr.appendChild(tdJugador);   // Agregar la celda 'Jugador' a la fila
                 tr.appendChild(tdPuntos);    // Agregar la celda 'Puntos' a la fila
                 tr.appendChild(tdVictorias);   // Agregar la celda 'Ganados' a la fila
+                tr.appendChild(tdActions);
 
                 tableBody.appendChild(tr); // Agregar la fila al cuerpo de la tabla
             });
